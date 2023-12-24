e96be8afc0c77bc0762312a425abe2d601a50672
hpanushan <hpanushan@gmail.com>
2023-12-24 01:05:34 -0500

    Added PUT and DELETE request methods to API

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -34,5 +34,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('movie/',include('movie_app.urls')),
+    path('api/v1/',include('movie_app.urls')),
 ]
